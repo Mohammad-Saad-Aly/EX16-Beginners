f5afdaca92283f85ac003a6f4d11fb71348a6c05
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-01-15 20:05:33 +0500

    new

---
+++ class 5/app.js
@@ -0,0 +1,49 @@
+// array
+
+// push // last main insert karta
+// pop // "" "" "" delete karta
+// shift // start se delete karta
+// unshift // start se insert karta
+
+
+// var arr = ['Saad',23,true,10.4]
+// arr.shift()
+// arr.unshift('Ali','Jalal')
+// arr.pop()
+// document.write(arr) 
+
+// arr.push("Ali",'Jalal')
+
+
+
+// document.write(arr[3]+" --- "+arr[0])
+// document.write(arr)
+
+// var arr = []
+// arr[0] = 'Saad'
+// arr[2] = 'Ali'
+// // arr.push('Ali')
+// document.write(arr[2])
+
+// var kisey;
+// document.write(kisey123)
+
+// document.write(false && true)
+
+// array k methods
+// slice
+// splice
+
+// var arr = ['Saad', 'Ali', 'Aisha', 'Kubra']
+// arr.splice(1,2)
+
+// document.write(arr)
+// var arr2 = arr.slice()
+// document.write(arr2)
+
+
+task :
+1 value lo input
+2
+3
+4
---
+++ class 6/app.js
@@ -0,0 +1,55 @@
+// var arr = []
+// arr.push('Saad','Ali')
+// arr.shift()
+// document.write(arr)
+// splice(1,0,'ali')
+
+// loops 
+
+// for loop
+// var name = 'Saad'
+// var i;
+// for(i = 0; i < 101; i++){
+//     document.write(name+"<br>")
+// }
+
+// var table = +prompt('Enter your number ?')
+// var table2 = +prompt("Enter your Number2")
+// for (var i = 1; i <= table2; i++) {
+
+//     document.write(table + " x " + i + " = " + table * i + "<br>")
+// }
+
+// while loop 
+
+// var i = 0;
+
+// while(i < 11){
+//     document.write(i+"<br>")
+//     i++
+// }
+
+// same table print karo jo for loop main kia tha ... 
+
+
+// do while loop
+// var i = 0
+// do {
+//     document.write(i)
+//     i++
+// } while (i < 11)
+
+
+// reverse counting || even numbers
+// for (var i = 10; i >= 0; i = i - 2) {
+//     document.write(i, "<br>")
+// }
+
+// infinite loop
+// for(var i = 2; i > 1; i++){
+//     console.log(i)
+// }
+
+
+
+
