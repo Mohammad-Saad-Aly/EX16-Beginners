94e66302266aa6113f15b8071fb00b6986e7653d
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-01-22 19:49:19 +0500

    functions id concept bulb on off

---
+++ class 8/app.js
@@ -0,0 +1,64 @@
+// function add() {
+//     var num1 = 5
+//     var num2 = 5
+//     return num1 + num2
+// }
+
+// document.write(add())
+
+// function add(a,b){
+//     return a + b 
+// }
+
+// document.write(add(4,10))
+
+
+// function check(){
+//     return 'Hello world'
+// }
+// console.log(check())
+
+
+// default parameters
+// function add(a=4,b=5){
+//     return a + b
+// }
+// console.log(add(10))
+
+
+// function check(){
+//     var name1 = 'Saad'   
+// }
+
+// console.log(name1)
+
+
+// function play(){
+//     alert('Play chal gaya.')
+// }
+
+// function showPara(){
+//     var show = 'Mohammad Saad Ali'
+//     document.getElementById('para').innerText = show 
+// }
+
+// function showImage(){
+//     var imageSrc = 'https://www.audi.com.pk/content/dam/nemo/models/audi-e-tron-gt/e-tron-gt/my-2022/NeMo-Carline-Startpage/1920-stage/1920x1920-audi-e-tron-gt-Stage_Desktop.jpg?imwidth=768';
+//     document.getElementById('img').src = imageSrc
+// }
+
+function onbulb() {
+    document.getElementById('img').src = './hashim/hashim.jpg'
+ }
+
+function offbulb() {
+    document.getElementById('img').src = './hashim/khula hua hashim.jpg'
+}
+
+
+
+
+
+
+
+
