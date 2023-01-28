7ff93dc4cdda85d68ea955c289226578db1e6560
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-01-28 20:12:56 +0500

    math methods head tail and calculator

---
+++ class 9/app.js
@@ -0,0 +1,65 @@
+// var arr2 = arr.lastIndexOf('Saad',3)
+// console.log(arr2)
+// var arr2 = arr.indexOf('Jalal')
+// console.log(arr2)
+
+// var naam = 'Ali'
+// console.log(naam.charAt(3))
+
+// indexOf
+// lastIndexOf
+// charAt
+// replace
+// Math.round
+// Math.ceil
+// Math.floor
+// Math.random
+
+
+// var text = 'Hello world Hello Saad Hello'
+// var newText = text.replace(/Hello/g,'Ali')
+// console.log(newText)
+
+// var num = 72.1
+// var newNum = Math.floor(num)
+// var newNum = Math.ceil(num)
+// var newNum = Math.round(num)
+// console.log(newNum)
+// var num = Math.random() * 4
+// console.log(num)
+
+
+
+// var pak = prompt('Enter Your toss') 
+// var ind = prompt('Enter Your toss') 
+// var toss = Math.random() * 2
+// var fix = Math.floor(toss)
+// if(fix == 0){
+//     document.write(pak+" win the toss")
+// }
+// else{
+//     document.write(ind+ ' win the toss')
+// }
+
+function abc(a) {
+    document.getElementById('inp').value += a
+
+}
+
+function sum() {
+    var a = document.getElementById('inp').value
+    document.getElementById('inp').value = eval(a)
+}
+
+function clr(){
+    document.getElementById('inp').value = ''
+}
+
+
+
+
+
+
+
+
+
