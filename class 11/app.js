8dd84d5fcd578c5314266294e9a60dbc8fe3b37e
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2023-02-05 19:56:27 +0500

    class 11

---
+++ class 11/app.js
@@ -0,0 +1,150 @@
+// function foo (){
+// document.getElementById('abc').className += ' big font'
+// document.getElementById('abc').style.backgroundColor = 'red'
+// var somepara = 'Hello world'
+// document.getElementById('showpara').innerText = somepara
+// }
+
+// var dayNames = ["Sunday", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// document.write(nameOfToday)
+
+// var date = new Date('12 sep, 1999')
+// var date1 = date.getTime()
+// console.log(date1) 
+
+// var milisec = new Date()
+// var getmilisec = milisec.getTime()  // miliseconds from jan 1, 1970
+// var secondmili = new Date('12 sep, 1999')
+// var secondmili2 = secondmili.getTime()   // miliseconds from 12 sep 1999
+// var minus = getmilisec - secondmili2  // minus kardiye miliseconds
+// var formula = minus / (1000 * 60 * 60 * 24 * 365)
+// document.write(Math.floor(formula))
+
+
+// var user = prompt('Enter name')
+// switch(user){
+//  case 'Saad':
+//  alert('han saad he');
+//  break;
+//  case 'Ali':
+//  alert('han ali he');
+//  break;
+//  default :
+//  alert('nikal ....')
+
+// }
+
+// function alled(){
+//   var abc = document.getElementsByTagName('p')
+//   for(var i = 0; i < abc.length; i++){
+//     abc[i].className = 'font'
+// abc[i].style.backgroundColor = 'red'
+// }
+// abc[0].style.color = 'red'
+// abc[2].style.color = 'red'
+// }
+
+// function alled(){
+//   var a = document.getElementById('abc')
+//   var b = a.getElementsByTagName('p')
+//   for(var i=0; i< b.length; i++){
+//     b[i].style.backgroundColor = 'red'
+//   }
+// }
+
+
+// for (var i = 1; i <= 100; i = i + 10) {
+//   for (var j = i; j < i + 10; j++){
+//     document.write(j," ")
+//   }
+//   document.write('<br>')
+// }
+
+
+
+// for(var i=1; i <=5; i++){
+//   for(var j=1; j <= i; j++){
+//     document.write(i,' ')
+//   }
+//   document.write('<br>')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var abc = date.
+// document.write(abc)
+// var abc = date.toString()
+// var copy = abc.slice(0,5)
+// document.write(copy)
